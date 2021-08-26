523f06bfe59b081b6e845e9468dcbd2a76a801d2
Duncan Hunter <duncan@duncanhunter.com.au>
2021-08-26 23:08:38 +1000

    update blog and styles with prose

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
   theme: {
     extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            blockquote: {
+              color: theme("colors.gray.300"),
+              borderLeftColor: theme("colors.blue.600"),
+            },
+            h2: {
+              color: theme("colors.gray.300"),
+            },
+            h3: {
+              color: theme("colors.gray.300"),
+            },
+            a: {
+              color: theme("colors.blue.600"),
+              "&:hover": {
+                color: theme("colors.blue.400"),
+              },
+            },
+          },
+        },
+      }),
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        coolGray: colors.coolGray,
-        gray: colors.trueGray
-      }
+        primary: colors.yellow,
+        gray: colors.trueGray,
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
